26d7be27fdfb387a02f77a898c02fc5c5e42319c
N2N <320369089@qq.com>
2022-09-22 20:00:24 +0800

    lx

--- mybatisplus/src/main/java/com/mybatisplus/mybatisplus/MybatisplusApplication.java
+++ mybatisplus/src/main/java/com/mybatisplus/mybatisplus/MybatisplusApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+///http://localhost:8080/swagger-ui.html#/   start web path
 @MapperScan("com.mybatisplus.mybatisplus.mapper")
 @SpringBootApplication
 public class MybatisplusApplication {
