0d3cf4fd6fcf50e8a9236b5cb1dab251643d20d9
Administrator <123>
2022-09-30 13:16:26 +0800

    123

---
+++ springboot-ds/src/test/java/com/fs/springbootds/SpringbootDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.springbootds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
