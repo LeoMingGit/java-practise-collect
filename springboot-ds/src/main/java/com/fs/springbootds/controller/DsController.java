50a42fcc02f434e9d78224fe0c99d4f20c224fe7
N2N <320369089@qq.com>
2022-10-07 13:21:13 +0800

    233

--- springboot-ds/src/main/java/com/fs/springbootds/controller/DsController.java
+++ springboot-ds/src/main/java/com/fs/springbootds/controller/DsController.java
@@ -25,7 +25,7 @@ public class DsController {
     public String StreamApi() {
 
         //https://blog.csdn.net/qq_27005889/article/details/121053641?spm=1001.2101.3001.6650.2&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-2-121053641-blog-124370414.pc_relevant_default&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-2-121053641-blog-124370414.pc_relevant_default&utm_relevant_index=3
-        // 流的方式操作数组
+        // 流的方式操作数组233
         List<String> strings = Arrays.asList("lily","bob","tom","cat","mom");
 
         List<String> res = strings.stream()
