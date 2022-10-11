148da8d409efa4cfc4bd59b00cce603f9c5ff894
Administrator <123>
2022-10-11 13:52:30 +0800

    lx

---
+++ tuling-sql/src/main/java/com/fs/tulingsql/TulingSqlApplication.java
@@ -0,0 +1,13 @@
+package com.fs.tulingsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulingSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TulingSqlApplication.class, args);
+    }
+
+}
---
+++ tuling-sql/src/test/java/com/fs/tulingsql/TulingSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.tulingsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulingSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
