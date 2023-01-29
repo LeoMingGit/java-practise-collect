9e4fdc039034b9da4a2bb0bd631e180fd3b55139
N2N <320369089@qq.com>
2023-01-29 19:26:18 +0800

    maven父子工程

---
+++ springboot-parent-son/parent-project/src/main/java/parent/project/test.java
@@ -0,0 +1,11 @@
+package parent.project;
+
+import son.project.son;
+
+public class test {
+
+    public static void main(String[] args) {
+        son s=new  son();
+        son.SayHello();
+    }
+}
---
+++ springboot-parent-son/son-project/src/main/java/son/project/son.java
@@ -0,0 +1,8 @@
+package son.project;
+
+public class son {
+    
+    public static void  SayHello(){
+        System.out.println("hello ,i am son project ");
+    }
+}
