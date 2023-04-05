378bec95db6c9bfb2648dfafc44ce68951b2074d
lixiang <320369089@qq.com>
2023-04-05 09:31:27 +0800

    dahua

--- restfull-api-dahua/src/main/java/com/restfull/restfullapi/controller/DahuController.java
+++ restfull-api-dahua/src/main/java/com/restfull/restfullapi/controller/DahuController.java
@@ -24,14 +24,14 @@ public class DahuController {
     public void  testCarCapture(@RequestBody String param, HttpServletRequest request, HttpServletResponse response) throws Exception {
         //System.out.println("+++++++++++++++++++接收到新增推送接口数据+++++++++++++++++++++");
 
-        param="+HHQVNS5HqR6aApWLS2r1YoL6Aod0Ujw7wY%2F4IyYxUi47W1v3%2FmYfvMjA6WH6WfCitvIG4b0C3uZBReoxU635veq4%2FV8I2qkOwxcX%2F7SPcQt9sAH8FtyHzDMEshrwF7m2avDh7cJd8%2FIdLjcE+Bpf+kV4kyTIYPD9NDB691UfCjqYzah1gHHR631fp6vXMCjgd4dqIKJYB3FjXiv1UrmIyaPG%2FMvJUpomeyamSBmgL9S2Rp+47LIB+OYNkf17j1Yk96yhkedr0hXv%2FCiD%2FBzsTNdTIgWYeTMISodwS034hW7jtl4ST%2Fj2Pb7hoLv1ENrP%2Fashl+1bIQi56CD%2F2nZ9VICdByYmMW8zt0pepJ5Bnai0LTMGYajUKXkLgLF%2F8VXobnE9Hn9vAoBeXWVbjlFH+0OKzEWWIzfzVpKtI+zmJlouynkWChlKrZNELorFShrcGnVJpCrWiPNtwcGlC8UdE7ans47G397MtqeYF9SzM1+ZtnYtnYW7uC0FtWHbuJUrlWcK120f689Ttj0qYyEJwhkLXR5PlIHG+sS1WXnY2rx7Bc7FsLopcxY4m7eGfFbAC24iVc1jhKte9mvIx%2FThAROz3B92Zd+HNWbGeVjJ33iTBv0k77Gt7PlLILiGwzwq9c94rn+K0NPWE%2F7f%2Fv77NKGjqTotkburwQ8zxjYzBYtiHaBr3wWPNr2OsPU5bHaGJ9Yk6LUd0vN7Ui2HYqWRtNT0VWh0EWY2V9NzBy2s7glujXnxHX2oL92NvHQyB0+iqbIeUN2oErmFy1XwFBQpjaGjDVLhTliqKUbptbrNAp%2FWr4ZHqZpRtEVpRSCxE4A9Ee1nTuMoJ8jcnke1Pzkr7NV4mD5zw0Vdzgu%2F4A2C2aHY1ke+wOXU6PEQbKKBF4BjRbhwfRWmuttZE0pZ+HAqg=%3D";
+     //   param="+HHQVNS5HqR6aApWLS2r1YoL6Aod0Ujw7wY%2F4IyYxUi47W1v3%2FmYfvMjA6WH6WfCitvIG4b0C3uZBReoxU635veq4%2FV8I2qkOwxcX%2F7SPcQt9sAH8FtyHzDMEshrwF7m2avDh7cJd8%2FIdLjcE+Bpf+kV4kyTIYPD9NDB691UfCjqYzah1gHHR631fp6vXMCjgd4dqIKJYB3FjXiv1UrmIyaPG%2FMvJUpomeyamSBmgL9S2Rp+47LIB+OYNkf17j1Yk96yhkedr0hXv%2FCiD%2FBzsTNdTIgWYeTMISodwS034hW7jtl4ST%2Fj2Pb7hoLv1ENrP%2Fashl+1bIQi56CD%2F2nZ9VICdByYmMW8zt0pepJ5Bnai0LTMGYajUKXkLgLF%2F8VXobnE9Hn9vAoBeXWVbjlFH+0OKzEWWIzfzVpKtI+zmJlouynkWChlKrZNELorFShrcGnVJpCrWiPNtwcGlC8UdE7ans47G397MtqeYF9SzM1+ZtnYtnYW7uC0FtWHbuJUrlWcK120f689Ttj0qYyEJwhkLXR5PlIHG+sS1WXnY2rx7Bc7FsLopcxY4m7eGfFbAC24iVc1jhKte9mvIx%2FThAROz3B92Zd+HNWbGeVjJ33iTBv0k77Gt7PlLILiGwzwq9c94rn+K0NPWE%2F7f%2Fv77NKGjqTotkburwQ8zxjYzBYtiHaBr3wWPNr2OsPU5bHaGJ9Yk6LUd0vN7Ui2HYqWRtNT0VWh0EWY2V9NzBy2s7glujXnxHX2oL92NvHQyB0+iqbIeUN2oErmFy1XwFBQpjaGjDVLhTliqKUbptbrNAp%2FWr4ZHqZpRtEVpRSCxE4A9Ee1nTuMoJ8jcnke1Pzkr7NV4mD5zw0Vdzgu%2F4A2C2aHY1ke+wOXU6PEQbKKBF4BjRbhwfRWmuttZE0pZ+HAqg=%3D";
 
         //System.out.println(param);
         param = URLDecoder.decode(param, "utf-8");
         param = param.replace(" ", "+");
        // System.out.println(param);
         String userAgent = request.getHeader("User-Agent");
-        userAgent="45833fad48aa095f6228cc979802fc9003f33d89186ac707561";
+      //  userAgent="45833fad48aa095f6228cc979802fc9003f33d89186ac707561";
         //System.out.printf("==================32行=================");
         //System.out.printf(userAgent);
         userAgent = userAgent.substring(userAgent.lastIndexOf("_") + 1, userAgent.length());
