10cf54eed587707725de504da28f2d50352a1450
N2N <320369089@qq.com>
2023-08-26 14:42:52 +0800

    1.加入生产环境，测试环境，开发环境配置文件 2.增加导入excel文件逻辑

---
+++ RuoYi-Vue-Admin/ruoyi-system/src/main/java/com/ruoyi/system/domain/dto/OrderMasterDto.java
@@ -0,0 +1,128 @@
+package com.ruoyi.system.domain.dto;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+
+public class OrderMasterDto {
+
+    @ExcelProperty("order_id")
+    private Integer orderId;
+
+    @ExcelProperty("buyer_name")
+    private String buyerName;
+
+    @ExcelProperty("buyer_phone")
+    private String buyerPhone;
+
+    @ExcelProperty("order_amount")
+    private BigDecimal orderAmount;
+
+    @ExcelProperty("status")
+    private Integer status;
+
+    @ExcelProperty("create_time")
+    private Date createTime;
+
+    @ExcelProperty("create_user")
+    private String createUser;
+
+    @ExcelProperty("update_time")
+    private Date updateTime;
+
+    @ExcelProperty("update_user")
+    private String updateUser;
+
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getBuyerPhone() {
+        return buyerPhone;
+    }
+
+    public void setBuyerPhone(String buyerPhone) {
+        this.buyerPhone = buyerPhone;
+    }
+
+    public BigDecimal getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(BigDecimal orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderMasterDto that = (OrderMasterDto) o;
+        return Objects.equals(orderId, that.orderId) && Objects.equals(buyerName, that.buyerName) && Objects.equals(buyerPhone, that.buyerPhone) && Objects.equals(orderAmount, that.orderAmount) && Objects.equals(status, that.status) && Objects.equals(createTime, that.createTime) && Objects.equals(createUser, that.createUser) && Objects.equals(updateTime, that.updateTime) && Objects.equals(updateUser, that.updateUser);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId, buyerName, buyerPhone, orderAmount, status, createTime, createUser, updateTime, updateUser);
+    }
+}
--- RuoYi-Vue-Admin/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/OrderMasterServiceImpl.java
+++ RuoYi-Vue-Admin/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/OrderMasterServiceImpl.java
@@ -1,14 +1,33 @@
 package com.ruoyi.system.service.impl;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.EasyExcelFactory;
+import com.alibaba.fastjson2.JSON;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.system.domain.OrderMaster;
+import com.ruoyi.system.domain.dto.OrderMasterDto;
 import com.ruoyi.system.domain.dto.OrderQueryDTO;
 import com.ruoyi.system.mapper.OrderMasterMapper;
 import com.ruoyi.system.service.IOrderMasterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.format.NumberFormat;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.converters.DefaultConverterLoader;
+import com.alibaba.excel.enums.CellExtraTypeEnum;
+import com.alibaba.excel.read.listener.PageReadListener;
+import com.alibaba.excel.read.listener.ReadListener;
+import com.alibaba.excel.read.metadata.ReadSheet;
+import com.alibaba.excel.util.ListUtils;
+import com.alibaba.fastjson2.JSON;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class OrderMasterServiceImpl implements IOrderMasterService {
@@ -58,12 +77,20 @@ public class OrderMasterServiceImpl implements IOrderMasterService {
 
     /**
      * 处理订单excel
-     * @param excel
+     * @param excelpath
      * @return
      */
     @Override
     public AjaxResult handleOrderExcel(String excelpath){
         System.out.println(excelpath);
+
+
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 同步读取会自动finish
+        List<OrderMasterDto> list = EasyExcel.read(excelpath).head(OrderMasterDto.class).sheet().doReadSync();
+        for (OrderMasterDto data : list) {
+            System.out.println(("读取到数据:{}"+JSON.toJSONString(data)));
+        }
+
 
         AjaxResult result= new AjaxResult();
 
